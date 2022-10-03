6d36bcea26d563a31f5b4ea816830136ebd71934
AleksandrTurlakov <aturlakov@ukr.net>
2022-10-03 23:42:29 +0300

    ДЗ №8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,25 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
 console.log(galleryItems);
+
+const render = galleryItems.reduce(
+  (acc, { description, original, preview }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`,
+  ''
+);
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('afterbegin', render);
+
+let galleryNew = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const dataForm = {};
+
+const form = document.querySelector('.feedback-form ');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+populateForm();
+
+function onFormInput(evt) {
+  dataForm[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log('Отправка формы');
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateForm(evt) {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedMessage) {
+    console.log(savedMessage);
+    console.log(dataForm);
+
+    dataForm.name = savedMessage.email;
+    dataForm.message = savedMessage.message;
+  }
+}
