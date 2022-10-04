013670703a829bc7662738888f8feaa2403a4a44
AleksandrTurlakov <aturlakov@ukr.net>
2022-10-04 05:55:42 +0300

    rewritten code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,11 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const dataForm = {};
+// const dataForm = {};
 
 const form = document.querySelector('.feedback-form ');
+const input = document.querySelector('input[name = email]');
+const textarea = document.querySelector('textarea[name = message]');
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
@@ -11,14 +13,15 @@ form.addEventListener('input', throttle(onFormInput, 500));
 populateForm();
 
 function onFormInput(evt) {
-  dataForm[evt.target.name] = evt.target.value;
+  //   dataForm[evt.target.name] = evt.target.value;
+  const dataForm = { email: input.value, message: textarea.value };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  //   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  console.log('Отправка формы');
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   evt.target.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
@@ -26,11 +29,12 @@ function onFormSubmit(evt) {
 function populateForm(evt) {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (savedMessage) {
+  if (!savedMessage) {
     console.log(savedMessage);
-    console.log(dataForm);
-
-    dataForm.name = savedMessage.email;
-    dataForm.message = savedMessage.message;
+    input.value = '';
+    textarea.value = '';
+  } else {
+    input.value = savedMessage.email;
+    textarea.value = savedMessage.message;
   }
 }
